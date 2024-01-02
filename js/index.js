912b4e629efa1533aa354f152983efbf4f1704e4
HordynskyiVitalii <145695512+HordynskyiVitalii@users.noreply.github.com>
2024-01-02 19:00:36 +0100

    Add

--- js/index.js
+++ js/index.js
@@ -1,49 +1,26 @@
-function validateForm() {
-    // Reset errors
-    document.getElementById('nameError').innerHTML = '';
-    document.getElementById('emailError').innerHTML = '';
-    document.getElementById('agreementError').innerHTML = '';
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.querySelector('.hero_video video');
+    const playButton = document.getElementById('buttonPlay');
 
-    let error = 0;
+    playButton.addEventListener('click', function() {
+        if (video.paused || video.ended) {
+            video.play();
+            playButton.innerHTML = '<svg class="icon_pause_bxs" height="84.521" width="84.521"><use href="./images/icons.svg#icon-pause-bxs"></use></svg>';
+        } else {
+            video.pause();
+            playButton.innerHTML = '<svg class="icon_play_bxs" height="84.521" width="84.521"><use href="./images/icons.svg#icon-bxs"></use></svg>';
+        }
+    });
 
-    // Validate name
-    let nameInput = document.getElementById('formName');
-    if (nameInput.value.trim() === '') {
-        document.getElementById('nameError').innerHTML = 'Name is required';
-        nameInput.classList.add('_error');
-        error++;
-    } else {
-        nameInput.classList.remove('_error');
-    }
+    video.addEventListener('ended', function() {
+        playButton.innerHTML = '<svg class="icon_play_bxs" height="84.521" width="84.521"><use href="./images/icons.svg#icon-bxs"></use></svg>';
+    });
 
-    // Validate email
-    let emailInput = document.getElementById('formEmail');
-    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(emailInput.value.trim())) {
-        document.getElementById('emailError').innerHTML = 'Invalid email';
-        emailInput.classList.add('_error');
-        error++;
-    } else {
-        emailInput.classList.remove('_error');
-    }
+    video.addEventListener('play', function() {
+        playButton.style.opacity = 0;
+    });
 
-    // Validate agreement
-    let agreementInput = document.getElementById('formAgreement');
-    if (!agreementInput.checked) {
-        document.getElementById('agreementError').innerHTML = 'You must agree to the terms';
-        agreementInput.classList.add('_error');
-        error++;
-    } else {
-        agreementInput.classList.remove('_error');
-    }
-
-    if (error === 0) {
-        // Form is valid, you can submit it or perform additional actions
-        alert('Form submitted successfully!');
-    } else {
-        // Form has errors, do not submit
-        alert('Please fix the errors before submitting');
-    }
-}
-
-
-
+    video.addEventListener('pause', function() {
+        playButton.style.opacity = 1;
+    });
+});
