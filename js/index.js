6616572677273f86c9bbe351750b6b9a1ba54f60
HordynskyiVitalii <145695512+HordynskyiVitalii@users.noreply.github.com>
2024-01-03 18:37:32 +0100

    add

--- js/index.js
+++ js/index.js
@@ -32,3 +32,111 @@ $(document).ready(function () {
     });
 });
 
+var form = document.getElementById("myForm");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("form_status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Thanks for your submission!";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
+
+// Make sure to run npm install @formspree/react
+// For more help visit https://formspr.ee/react-help
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xayrwrdo");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
+  return (
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
+  );
+}
+function App() {
+  return (
+    <ContactForm />
+  );
+}
+export default App;
+
+ /* paste this line in verbatim */
+ window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+ /* customize formbutton below*/     
+ formbutton("create", {
+   action: "https://formspree.io/f/xayrwrdo",
+   title: "How can we help?",
+   fields: [
+     { 
+       type: "email", 
+       label: "Email:", 
+       name: "email",
+       required: true,
+       placeholder: "plantych88@gmail.com"
+     },
+     {
+       type: "textarea",
+       label: "Message:",
+       name: "message",
+       placeholder: "What's on your mind?",
+     },
+     { type: "submit" }      
+   ],
+   styles: {
+     title: {
+       backgroundColor: "gray"
+     },
+     button: {
+       backgroundColor: "gray"
+     }
+   }
+ });
